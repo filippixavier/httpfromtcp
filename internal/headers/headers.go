24c67138a0548d45131d9fe0617df235990210e8
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2025-09-25 21:57:34 +0200

    Ensure that header is rfc compliant

---
+++ internal/headers/headers.go
@@ -0,0 +1,53 @@
+package headers
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	headers := make(map[string]string)
+
+	return headers
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	str := string(data)
+
+	lines := strings.Split(str, "\r\n")
+	read := 0
+
+	invalidReg, err := regexp.Compile("[^a-zA-Z0-9!#$%&'*+-.^_`|~]")
+
+	if err != nil {
+		return 0, false, err
+	}
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		parts := strings.Split(line, ": ")
+
+		if len(parts) != 2 {
+			return read, false, fmt.Errorf("invalid header field")
+		}
+
+		if strings.Contains(parts[0], " ") || strings.Contains(parts[1], " ") {
+			return read, false, fmt.Errorf("invalid space header")
+		}
+
+		if invalidReg.MatchString(parts[0]) {
+			return read, false, fmt.Errorf("header contain invalid charaters")
+		}
+
+		h[strings.ToLower(parts[0])] = parts[1]
+		read += len(line) + 2
+	}
+
+	return read, false, nil
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,46 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid single header (lowercase)
+	headers = NewHeaders()
+	data = []byte("live-test: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["live-test"])
+	assert.Equal(t, 28, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid header (wrong character)
+	headers = NewHeaders()
+	data = []byte("H@st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
